fc44042ab9f7e546d13899394d2dc9b5fa4f1564
aiperi <aiperi.damirkyzy@gmail.com>
2023-04-04 21:06:22 +0600

    feature/GSPINT-1221

---
+++ src/VisaPayment.java
@@ -0,0 +1,11 @@
+public class VisaPayment {
+    public void payViaVisa(String bank){
+
+        System.out.println("Some payment from: " + bank);
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
